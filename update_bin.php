34f4402b9c284647e325a006271d1ae19f9cde21
channaveer <channaveer888@gmail.com>
2017-01-10 13:31:05 +0530

    Added Condition to check whether Bin is not N/A, If N/A then dont change the details of the slabs.

--- update_bin.php
+++ update_bin.php
@@ -40,32 +40,35 @@ if($scanned_barcode == '' || $barcode_length == 0){
 
         $bin_name = $scanned_barcode;
         if(count($_SESSION['scanned_slabs']) > 0){
-            $bin_details = mysqli_fetch_assoc(mysqli_query($link,"SELECT `id` FROM `bins` WHERE name='$bin_name'"));
-            $count = 0;
-            $final_inventory_master_details = array();
-            foreach($_SESSION['scanned_slabs'] as $key=>$unique_values){
-                $inventory_master_query =   "SELECT im.sku, bi.isipl_number, b.name as bin_name"
-                                            . " FROM inventory_master im"
-                                            . " JOIN bins b ON b.id = im.binid"
-                                            . " JOIN block_inventory bi ON bi.id = im.blockinventoryid"
-                                            . " WHERE im.uniqueNumber = '$unique_values'";
-                $inventory_master_details = mysqli_query($link, $inventory_master_query);
+            $bin_details = mysqli_fetch_assoc(mysqli_query($link,"SELECT `id`,`name` FROM `bins` WHERE name='$bin_name'"));
+			if($bin_details['name'] != 'N/A'){
+				$count = 0;
+				$final_inventory_master_details = array();
+				foreach($_SESSION['scanned_slabs'] as $key=>$unique_values){
+					$inventory_master_query =   "SELECT im.sku, bi.isipl_number, b.name as bin_name"
+												. " FROM inventory_master im"
+												. " JOIN bins b ON b.id = im.binid"
+												. " JOIN block_inventory bi ON bi.id = im.blockinventoryid"
+												. " WHERE im.uniqueNumber = '$unique_values'";
+					$inventory_master_details = mysqli_query($link, $inventory_master_query);
 
-                $currenDateTime = date('Y-m-d H:i:s');
-                $im_result = mysqli_query($link,"UPDATE `inventory_master` SET `binId` = '$bin_details[id]', `isStockStatusVerified` = 1, `lastVerifiedDate`='$currenDateTime' WHERE `uniqueNumber` = '$unique_values'");
-                if($im_result){
-                    //Updated successfully now store the details of the "inventory_master" table to dispaly
-                    while($row = mysqli_fetch_assoc($inventory_master_details)){
-                        $final_inventory_master_details[$key]['sku']            = $row['sku'];
-                        $final_inventory_master_details[$key]['isipl_number']   = $row['isipl_number'];
-                        $final_inventory_master_details[$key]['old_bin_name']   = $row['bin_name'];
-                        $final_inventory_master_details[$key]['new_bin_name']   = $bin_name;
-                    }
-                    $count++;
-                }
-            }
-            echo '<div class="text-success">Updated '.$count.' slabs with bin number to '.$bin_name.'</div> <br />';
-
+					$currenDateTime = date('Y-m-d H:i:s');
+					$im_result = mysqli_query($link,"UPDATE `inventory_master` SET `binId` = '$bin_details[id]', `isStockStatusVerified` = 1, `lastVerifiedDate`='$currenDateTime' WHERE `uniqueNumber` = '$unique_values'");
+					if($im_result){
+						//Updated successfully now store the details of the "inventory_master" table to dispaly
+						while($row = mysqli_fetch_assoc($inventory_master_details)){
+							$final_inventory_master_details[$key]['sku']            = $row['sku'];
+							$final_inventory_master_details[$key]['isipl_number']   = $row['isipl_number'];
+							$final_inventory_master_details[$key]['old_bin_name']   = $row['bin_name'];
+							$final_inventory_master_details[$key]['new_bin_name']   = $bin_name;
+						}
+						$count++;
+					}
+				}
+				echo '<div class="text-success">Updated '.$count.' slabs with bin number to '.$bin_name.'</div> <br />';
+			}else{
+				echo '<div class="text-danger">Bin Not Selected</div> <br />';
+			}
             ?>
             <a href="<?php echo $base_url; ?>home.php" class="text-danger">Click Here</a> to go to dashboard <br /><br />
             <div class="table-responsive">
